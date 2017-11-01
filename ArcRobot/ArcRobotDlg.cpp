293c56b75cd3ccb0685b4c71df1b4806991b260b
QinChen686 <1403453010@qq.com>
2017-11-01 15:14:11 +0800

    add ok button

--- ArcRobot/ArcRobotDlg.cpp
+++ ArcRobot/ArcRobotDlg.cpp
@@ -64,6 +64,7 @@ BEGIN_MESSAGE_MAP(CArcRobotDlg, CDialogEx)
 	ON_WM_SYSCOMMAND()
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDOK, &CArcRobotDlg::OnBnClickedOk)
 END_MESSAGE_MAP()
 
 
@@ -154,3 +155,10 @@ HCURSOR CArcRobotDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
+
+
+void CArcRobotDlg::OnBnClickedOk()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	CDialogEx::OnOK();
+}
